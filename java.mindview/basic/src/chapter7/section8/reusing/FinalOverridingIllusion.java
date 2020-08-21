fcc33056c7c604ec78dd5c5461005cb5414cbff6
Aike_Zhang <15826061916@139.com>
2020-08-21 15:28:29 +0800

    Add section4-8

--- java.mindview/basic/src/chapter7/section8/reusing/FinalOverridingIllusion.java
+++ java.mindview/basic/src/chapter7/section8/reusing/FinalOverridingIllusion.java
@@ -2,8 +2,20 @@ package chapter7.section8.reusing;
 
 /**
  * It only looks like you can override a private or private final method
+ *
  * @author zhanghua
  * @date 2020/8/18
  */
 public class FinalOverridingIllusion {
+
+}
+class WithFinals{
+    // Identical to "private" alone
+    private final void f(){
+        System.out.println("WithFinals.f()");
+    }
+    // Also automatically "final"
+    private void g(){
+        System.out.println("WithFinals.g()");
+    }
 }
