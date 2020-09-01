506fa20198afae9e05aefa1fa6236f3178705a6b
zhanghua <15826061916@139.com>
2020-09-01 15:55:00 +0800

    feat: chapter8:section3

--- java.mindview/basic/src/chapter8/section2/polymorphism/shape/RandomShapeGenerator.java
+++ java.mindview/basic/src/chapter8/section2/polymorphism/shape/RandomShapeGenerator.java
@@ -1,8 +1,26 @@
 package chapter8.section2.polymorphism.shape;
 
+import java.util.Random;
+
 /**
+ * A "factory" that randomly creates shapes.
+ *
  * @author zhanghua
  * @date 2020/8/31
  */
 public class RandomShapeGenerator {
+    private Random random = new Random(47);
+
+    public Shape next() {
+        switch (random.nextInt(3)) {
+            case 0:
+                return new Circle();
+            case 1:
+                return new Square();
+            case 2:
+                return new Triangle();
+            default:
+                return new Circle();
+        }
+    }
 }
